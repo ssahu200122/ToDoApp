b283586a7a820ffe5866e604ba337452e5aff218
sourabh sahu <ssahu200122@gmail.com>
2026-06-21 01:02:13 +0530

    implement task management features: add task, delete task, and toggle completion state

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from PySide6.QtWidgets import QApplication,QMainWindow
+from PySide6.QtWidgets import QApplication,QMainWindow,QVBoxLayout,QWidget,QPushButton,QHBoxLayout,QLineEdit,QListWidget,QListWidgetItem
 import sys
+
+from PySide6.QtCore import Qt
 
 app = QApplication(sys.argv)
 
@@ -11,7 +13,69 @@ class MainWindow(QMainWindow):
         self.resize(600,400)
 
 
+        outer = QVBoxLayout()
+        inner = QHBoxLayout()
+
+        # add button and input
+        addBtn = QPushButton("Add")
+        addBtn.clicked.connect(self.add_task)
+
+        deleteBtn = QPushButton("delete")
+        deleteBtn.clicked.connect(self.delete_task)
+
+        self.inputLine = QLineEdit(placeholderText="Enter the task")
+        self.inputLine.returnPressed.connect(self.add_task)
+
+        inner.addWidget(addBtn)
+        inner.addWidget(self.inputLine)
+        inner.addWidget(deleteBtn)
+
+        outer.addLayout(inner)
+
+
+        self.list_widget = QListWidget()
+        self.list_widget.itemChanged.connect(self.item_toggled)
+        outer.addWidget(self.list_widget)
+
+
+        container = QWidget()
+        container.setLayout(outer)
+
+        self.setCentralWidget(container)
+    
+    def add_task(self):
+        task = self.inputLine.text()
+
+        if task != "":
+            task_widget = QListWidgetItem(task)
+            task_widget.setFlags(task_widget.flags() | Qt.ItemIsUserCheckable)
+            task_widget.setCheckState(Qt.Unchecked)
+
+            self.list_widget.addItem(task_widget)
+            self.inputLine.clear()
+
+    def delete_task(self):
+        # print("Delete...")
+        row = self.list_widget.currentRow()
+
+        if row != -1:
+            self.list_widget.takeItem(row)
+
+    def item_toggled(self,item):
+        font = item.font()
+        if item.checkState() == Qt.Checked:
+            font.setStrikeOut(True)
+            item.setFont(font)
+        else:
+            font.setStrikeOut(False)
+            item.setFont(font)
+            
+
+        
+
+
 window = MainWindow()
 window.show()
+
 
 sys.exit(app.exec())
